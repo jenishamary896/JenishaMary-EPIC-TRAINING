3763efabea71aae1dd04c326fbad85a63f092410
jenishamary896 <jenishamary896@gmail.com>
2026-08-26 18:56:11 +0530

    Add Java class with methods for display and return value

---
+++ Java/WithReturnType.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+    void display(){
+        System.out.println("Hello World");
+    }
+    int mydata(){
+        return 70;
+    }
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.display();
+		System.out.println(m.mydata());
+	}
+}
