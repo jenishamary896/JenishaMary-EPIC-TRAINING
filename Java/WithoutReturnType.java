18d7a318775c37b9af55502c8a39f6f054a7ab27
jenishamary896 <jenishamary896@gmail.com>
2026-08-26 18:55:35 +0530

    Add WithoutReturnType.java with display method

---
+++ Java/WithoutReturnType.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+    void display(){
+        System.out.println("Hello World");
+    }
+	public static void main(String[] args) {
+		Main m = new Main();
+		m.display();
+	}
+}
