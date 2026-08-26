d690462735834baab0ae0e903b4f0c4d6bc54086
jenishamary896 <jenishamary896@gmail.com>
2026-08-26 18:48:44 +0530

    Add SubarraySumWithAdditionalElement.java implementation

---
+++ Java/SubarraySumWithAdditionalElement.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        int prv =0 , next = 0 ;
+        while(prv<n){
+            int sum = 0;
+            for (int i= prv; i <= next;i++){
+                sum +=arr[i];
+            }
+            if (sum >= target){
+                prv++;
+                next = prv;
+                continue;
+            }
+            for (int j = next+1;j < n ; j++){
+                if (sum + arr[j] == target){
+                    for (int k = prv; k <= next;k++){
+                       System.out.print(arr[k]+" ");
+                    }
+                    System.out.print(arr[j]);
+                }
+            }
+            System.out.println();
+            next++;
+        }
+    }
+}
