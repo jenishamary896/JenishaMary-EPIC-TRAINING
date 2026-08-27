0501d0de06ac5dc53fc172b3bd97562d9e01e4b1
jenishamary896 <jenishamary896@gmail.com>
2026-08-27 11:48:52 +0530

    Implement matrix addition in MatrixAdd.java

---
+++ Java/MatrixAdd.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int rows=sc.nextInt();
+	int cols=sc.nextInt();
+	int[][] arr=new int[rows][cols];
+	int[][] arr1=new int[rows][cols];
+
+	for(int i=0;i<rows;i++){
+	   for(int j=0;j<cols;j++){
+	     arr[i][j]=sc.nextInt(); 
+    	}
+	}
+		for(int i=0;i<rows;i++){
+	   for(int j=0;j<cols;j++){
+	     arr1[i][j]=sc.nextInt(); 
+    	}
+	}
+		for(int i=0;i<rows;i++){
+	   for(int j=0;j<cols;j++){
+	     arr1[i][j]=arr[i][j]+arr1[i][j]; 
+	      System.out.print(arr1[i][j]+" ");
+    	}
+    	System.out.println();
+	}
+	
+	      
+	    
+	    
+	
+	}
+}
