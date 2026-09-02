c5df4b4bc9efcd759abdc4a5ae269912111bd456
jenishamary896 <jenishamary896@gmail.com>
2026-09-02 11:01:58 +0530

    Add distinct character printer in distinct.java
    Implement a program to print distinct characters from input string.

---
+++ String/distinct.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String str = in.nextLine();//abcd
+		int[] count = new int[26];
+	
+		for(int i=0;i<str.length();i++){
+		    int val = str.charAt(i) - 97;
+		    if(str.charAt(i)>=65&&str.charAt(i)<=90){
+		         val=str.charAt(i)-65;
+		    }
+		    count[val]++;
+		}
+		System.out.println();
+
+		for(int i=0;i<str.length();i++){
+		    int val = str.charAt(i) - 97;
+		     if(str.charAt(i)>=65&&str.charAt(i)<=90){
+		         val=str.charAt(i)-65;
+		    }
+		    if(count[val]==1){
+		        System.out.println(str.charAt(i));
+		    }
+		    if(count[val]>1){
+		        count[val]=0;
+		    }
+		}
+	}
+}
