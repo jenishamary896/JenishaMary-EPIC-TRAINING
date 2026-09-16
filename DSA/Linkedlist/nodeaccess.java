fd93300f40676cea2ab1f6303ed1e2cbb5b569a9
jenishamary896 <jenishamary896@gmail.com>
2026-09-16 15:21:43 +0530

    Update nodeaccess.java

--- DSA/Linkedlist/nodeaccess.java
+++ DSA/Linkedlist/nodeaccess.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 class Node{
     int data;
     Node address;
-     Node head = null,prev=null;
+    Node head = null,prev=null;
     
     Node(int data,Node add){
         this.data=data;
         this.address = add;
     }
+    
     Node(){
         
     }
-void insertData(Scanner in){
-System.out.println("Enter the no of Data: ");
+    
+    
+    void insertData(Scanner in){
+        System.out.println("Enter the no of Data: ");
             int n = in.nextInt();//3-->10,20,30
             for(int i=0;i<n;i++){
                 int val = in.nextInt();//10
@@ -27,21 +30,46 @@ System.out.println("Enter the no of Data: ");
                     prev=obj;
                 }
             }
-        }
-     void displayData(){
-        Node temp = head;
+    }
+    
+    void displayData(){
+        	Node temp = head;
     		while(temp!=null){
 		    System.out.println(temp.data);//40
 		    temp=temp.address;//null
 		}
     }
-   }
+    
+    void insertANode(Scanner in){
+        System.out.println("Enter the position");
+        int n = in.nextInt();//2
+        System.out.println("Enter the value: ");
+        int val = in.nextInt();//55
+        Node newNode = new Node(val,null);
+        Node temp = head;//1000
+        for(int i=0;i<n-2;i++){
+            temp=temp.address;
+        }
+        newNode.address = temp.address;
+        temp.address = newNode;
+    }
+    
+    
+}
 public class Main
 {
 	public static void main(String[] args) {
+	    
+	 
 	    Scanner in = new Scanner(System.in);
-	    Node node =new Node();
-        node.insertData(in); 
+	    
+	    
+	    
+	    Node node = new Node();
+	    
+		node.insertData(in);
         node.displayData();
-}
+        node.insertANode(in);
+        node.displayData();
+	}
 }
