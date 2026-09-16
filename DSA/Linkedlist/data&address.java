bbe3570a0adf36d64a3d7f4161586a8d9054372b
jenishamary896 <jenishamary896@gmail.com>
2026-09-16 12:43:05 +0530

    Implement linked list with Node class

---
+++ DSA/Linkedlist/data&address.java
@@ -0,0 +1,23 @@
+class Node{
+    int data;
+    Node address;
+    
+    Node(int data,Node add){
+        this.data=data;
+        this.address = add;
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Node obj1 = new Node(10,null);
+		Node head = obj1;
+		Node obj2 = new Node(20,null);
+		obj1.address=obj2;
+		Node obj3 = new Node(30,null);
+		obj2.address=obj3;
+		Node obj4 = new Node(40,null);
+        obj3.address=obj4;
+		System.out.println(head.address.address.address.data);
+	}
+}
