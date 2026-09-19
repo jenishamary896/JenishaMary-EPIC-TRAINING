bd59e8aa87861bc3bfa634d21ea4547187f024f1
jenishamary896 <jenishamary896@gmail.com>
2026-09-19 12:08:08 +0530

    Refactor linked list node access methods
    Refactored methods to use 'Scanner sc' instead of 'Scanner in' for consistency. Added methods for inserting at head and tail, and deleting nodes.

--- DSA/Linkedlist/nodeaccess.java
+++ DSA/Linkedlist/nodeaccess.java
@@ -1,25 +1,20 @@
 import java.util.Scanner;
-
 class Node{
     int data;
     Node address;
-    Node head = null,prev=null;
-    
-    Node(int data,Node add){
+    Node head=null,prev=null;
+    Node(int data,Node address){
         this.data=data;
-        this.address = add;
+        this.address=address;
     }
-    
     Node(){
         
     }
-    
-    
-    void insertData(Scanner in){
+    void insertData(Scanner sc){
         System.out.println("Enter the no of Data: ");
-            int n = in.nextInt();//3-->10,20,30
+            int n = sc.nextInt();//3-->10,20,30
             for(int i=0;i<n;i++){
-                int val = in.nextInt();//10
+                int val = sc.nextInt();//10
                 Node obj = new Node(val,null);
                 if(head==null){
                     head = obj;
@@ -39,37 +34,135 @@ class Node{
 		    temp=temp.address;//null
 		}
     }
-    
-    void insertANode(Scanner in){
-        System.out.println("Enter the position");
-        int n = in.nextInt();//2
-        System.out.println("Enter the value: ");
-        int val = in.nextInt();//55
-        Node newNode = new Node(val,null);
-        Node temp = head;//1000
+        void insertValue(Scanner sc){
+        System.out.println("enter the position:");
+        int n=sc.nextInt();
+        System.out.println("enter the value:");
+        int val=sc.nextInt();
+        Node newNode=new Node(val,null);
+        Node temp=head;
         for(int i=0;i<n-2;i++){
             temp=temp.address;
         }
-        newNode.address = temp.address;
-        temp.address = newNode;
+        newNode.address=temp.address;
+        temp.address=newNode;
     }
-    
-    
+void insertHead(Scanner sc){
+    System.out.println("Enter value:");
+    int val = sc.nextInt();
+
+    Node newNode = new Node(val, null);
+
+    newNode.address = head;
+    head = newNode;
 }
-public class Main
-{
-	public static void main(String[] args) {
-	    
-	 
-	    Scanner in = new Scanner(System.in);
-	    
-	    
-	    
-	    Node node = new Node();
-	    
-		node.insertData(in);
-        node.displayData();
-        node.insertANode(in);
-        node.displayData();
-	}
+void insertTail(Scanner sc){
+    System.out.println("Enter value:");
+    int val = sc.nextInt();
+
+    Node newNode = new Node(val, null);
+
+
+    if(head == null){
+        head = newNode;
+        return;
+    }
+
+    Node temp = head;
+
+    while(temp.address != null){
+        temp = temp.address;
+    }
+
+    temp.address = newNode;
 }
+void deleteValue(Scanner sc){
+    System.out.println("Enter the position:");
+    int n = sc.nextInt();
+
+    Node temp = head;
+    for(int i = 0; i < n - 2; i++){
+        temp = temp.address;
+    }
+    temp.address = temp.address.address;
+}
+void deleteHead(){
+    
+    if(head == null){
+        System.out.println("List is empty");
+        return;
+    }
+
+    head = head.address;
+    System.out.println("head deleted");
+}
+void deleteTail(){
+    if(head == null){
+        System.out.println("List is empty");
+        return;
+    }
+
+    if(head.address == null){
+        head = null;
+        return;
+    }
+
+    Node temp = head;
+
+    while(temp.address.address != null){
+        temp = temp.address;
+    }
+
+    temp.address = null;
+    System.out.println("tail deleted");
+}
+
+}
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        Node node=new Node();
+        System.out.println("1.insert data 2.display data 3.insert value 4.insert head 5.insert tail 6.delete value 7.delete head 8.delete tail");
+        while(true){
+        int n=sc.nextInt();
+        switch(n){
+            case 1:{
+               node.insertData(sc);
+               break;
+            }
+            case 2:{
+                node.displayData(); 
+                break;
+            }
+            case 3:{
+               node.insertValue(sc);
+               break;
+            }
+            case 4:{
+               node.insertHead(sc);
+               break;
+            }
+            case 5:{
+             node.insertTail(sc);
+              break;
+            }
+            case 6:{
+            node.deleteValue(sc);
+            break;
+            }
+            case 7:{
+             node.deleteHead();
+              break;
+            }
+            case 8:{
+            node.deleteTail();
+            break;
+            }
+          }
+            
+        }
+    }
+}
+      
+
+    
